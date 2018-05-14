bc5ea8ecfe8623ba0e2077db6a502a84721e03fd
Josh Mulloy <joshmulloy1@gmail.com>
2018-05-14 17:43:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var a=1, b=80
+function add(a, b) {
+  return (a+b)
+}
+function subtract(b, a) {
+  return (b-a)
+}
+function multiply(a, b) {
+  return (a*b)
+}
+function divide(b, a) {
+  return(b/a)
+}
+var number = 5 
+function inc(n) {
+  return ++n
+}
+function dec(n) {
+  return --n
+}
+function parseInt(a, b) {
+  return (a, b)
+}
